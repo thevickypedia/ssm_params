70b72c9a3c319421b7ef236d0f6fe89b83f1b130
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-05 10:03:11 -0500

    dedicated functions for each behavior

--- ssm.py
+++ ssm.py
@@ -1,23 +1,32 @@
 import boto3
+from pprint import pprint
 
 client = boto3.client('ssm')
 
-response_put = client.put_parameter(
-    Name='string',
-    Description='string',
-    Value='string',
-    Type='String'|'StringList'|'SecureString',
-)
 
-print(response_put)
+def put_param():
+    response_put = client.put_parameter(
+        Name='name',
+        Value='value',
+        Type='type'
+    )
+    return response_put
 
-response_get = client.get_parameter(
-    Name='string',
-    WithDecryption=True|False
-)
 
-print(response_get)
+def get_param():
+    response_get = client.get_parameter(Name='name', WithDecryption=True)
+    param = response_get['Parameter']
+    ret = {}
+    name = param['Name']
+    val = param['Value']
+    ret[name] = val
+    return ret
 
-response_del = client.delete_parameter(Name='string')
 
-print(response_del)
+def delete_param():
+    response_delete = client.delete_parameter(Name='name')
+    return response_delete
+
+
+if __name__ == '__main__':
+    pprint(delete_param())

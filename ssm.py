1e27e28f3cec61b38c410197f252b379ff2e16ad
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-05 10:05:13 -0500

    get user input for each behavior

--- ssm.py
+++ ssm.py
@@ -5,16 +5,30 @@ client = boto3.client('ssm')
 
 
 def put_param():
+    name = input('Enter the name of the parameter you wish to add:\n')
+    value = input(f'Enter the value for {name}:\n')
+    typ = int(input('Enter the type you wish to save the parameter as:\n1. String\n2. StringList\n3. SecureString\n'))
+    if typ == 1:
+        typ = 'String'
+    elif typ == 2:
+        typ = 'StringList'
+    elif typ == 3:
+        typ = 'SecureString'
+    else:
+        print('Enter either 1 or 2 or 3, its that simple.')
+        exit(0)
+
     response_put = client.put_parameter(
-        Name='name',
-        Value='value',
-        Type='type'
+        Name=name,
+        Value=value,
+        Type=typ
     )
     return response_put
 
 
 def get_param():
-    response_get = client.get_parameter(Name='name', WithDecryption=True)
+    name = input('Enter the name of the parameter you wish to retrieve (case sensitive):\n')
+    response_get = client.get_parameter(Name=name, WithDecryption=True)
     param = response_get['Parameter']
     ret = {}
     name = param['Name']
@@ -24,7 +38,9 @@ def get_param():
 
 
 def delete_param():
-    response_delete = client.delete_parameter(Name='name')
+    name = input('Enter the name of the parameter you wish to DELETE (case sensitive):\n')
+    response_delete = client.delete_parameter(Name=name)
+
     return response_delete
 
 

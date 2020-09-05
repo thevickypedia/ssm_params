ce9cad99dbefe3d566668e9abc067462f1e25796
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-05 10:00:40 -0500

    get and delete as per ssm guide

--- ssm.py
+++ ssm.py
@@ -2,11 +2,22 @@ import boto3
 
 client = boto3.client('ssm')
 
-response = client.put_parameter(
+response_put = client.put_parameter(
     Name='string',
     Description='string',
     Value='string',
     Type='String'|'StringList'|'SecureString',
 )
 
-print(response)
+print(response_put)
+
+response_get = client.get_parameter(
+    Name='string',
+    WithDecryption=True|False
+)
+
+print(response_get)
+
+response_del = client.delete_parameter(Name='string')
+
+print(response_del)
